9940e6263006e2c5a72dac06d292a50b2cb57e55
keykode20 <omryemilyohassan@gmail.com>
2019-07-09 00:06:33 +0200

    buona partenza

---
+++ src/main/java/hello/controller/TestController.java
@@ -0,0 +1,32 @@
+package hello.controller;
+
+import hello.model.User;
+import hello.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @Autowired private UserRepository repository;
+
+    @RequestMapping(value = "/add")
+    public String add(){
+        User n = new User();
+        n.setName("sassucci");
+        n.setEmail("tarrannoni@gmai..com");
+        repository.save(n);
+        return "added.html";
+    }
+
+    @RequestMapping(value = "/viewAll")
+    public String viewall( Model model){
+        Iterable<User> all = repository.findAll();
+        User next = all.iterator().next();
+        model.addAttribute("all", next.getEmail());
+        return "viewAll.html";
+    }
+
+}
